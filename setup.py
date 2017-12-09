9cc01aeff1d298035e818d6351f378b6d2968498
Rick Copeland <rick@arborian.com>
2017-12-09 17:55:36 -0500

    Update metadata

--- setup.py
+++ setup.py
@@ -7,18 +7,21 @@ setup(
     version_format='{tag}.dev{commitcount}',
     setup_requires=['setuptools-git-version'],
     description='SMART on FHIR client for Python',
-    long_description='',
+    # long_description='',
     classifiers=[
         "Programming Language :: Python",
     ],
     author='Rick Copeland',
     author_email='rick@arborian.com',
-    url='',
+    url='https://github.com/Arborian/fhirstorm',
     keywords='',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
+    install_requires=[
+        'requests_oauthlib',
+        'PyJWT',
+    ],
     tests_require=[],
     entry_points="""
     """)
